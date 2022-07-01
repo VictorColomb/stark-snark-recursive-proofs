4a86c63f155e8beff4993a4adf769982912613ce
Victor Colomb <victor.colomb@student.isae-supaero.fr>
2022-07-01 11:45:56 +0200

    script fixes

--- compile.sh
+++ compile.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 RED='\033[0;31m'
-
 if [[ $# -lt 1 ]]
 then
     echo -e "${RED}[!] Usage: $0 <circuit_name (omit the extension)>"
@@ -12,7 +11,7 @@ TARGET=$1
 # compile circom
 rm -rf ${TARGET}_cpp
 rm -f ${TARGET}.r1cs
-circom ${TARGET}.circom --r1cs --sym --c
+circom ${TARGET}.circom -p bls12381 --r1cs --sym --c
 [ ! -f ${TARGET}.r1cs ] && exit 1
 [ ! -d ${TARGET}_cpp ] && exit 1
 
@@ -22,4 +21,3 @@ cd ${TARGET}_cpp
 make
 cd ..
 ${TARGET}_cpp/${TARGET} input.json witness.wtns
-[ ! -f witness.wtns ] && exit 1
